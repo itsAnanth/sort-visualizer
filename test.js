af54847a1c2e44af62de35d9d5f4676ed3da38ce
EJBEAN <64283902+itsAnanth@users.noreply.github.com>
2021-11-28 15:40:52 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,27 +1,38 @@
 const root = document.getElementById('root');
-const speed = document.getElementById('speed')
+// const speed = document.getElementById('speed')
 const size = document.getElementById('size');
 const start = document.getElementById('start');
-const random = document.getElementById('random');
-const reverseBtn = document.getElementById('reverse');
+const select = document.getElementById('algs');
+const generate = document.getElementById('gen');
+const algorithms = [bubbleSort, insertionSort];
 
-reverseBtn.addEventListener('click', () => {
-    const array = getArray(size.value? size.value : Math.floor(Math.random() * 100));
-    array.sort((a, b) => a.value - b.value);
-    display(array);
-    reverse(array);
+let toSort = null;
+// const random = document.getElementById('random');
+// const reverseBtn = document.getElementById('reverse');
 
-});
-random.addEventListener('click', () => {
-    const array = getArray(Math.floor(Math.random() * 100));
-    display(array);
-    sort(array);
+
+// var randomColor = () => "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+// reverseBtn.addEventListener('click', () => {
+//     const array = getArray(size.value? size.value : Math.floor(Math.random() * 100));
+//     array.sort((a, b) => a.value - b.value);
+//     display(array);
+//     reverse(array);
+
+// });
+// random.addEventListener('click', () => {
+//     const array = getArray(Math.floor(Math.random() * 100));
+//     display(array);
+//     sort(array);
+// })
+
+generate.addEventListener('click', () => {
+    toSort = getArray(size.value ? size.value : 10);
+    display(toSort);
 })
-
 start.addEventListener('click', () => {
-    const array = getArray(size.value ? size.value : 10);
+    const array = !toSort ? getArray(size.value ? size.value : 10) : toSort;
     display(array);
-    sort(array);
+    algorithms[select.value](array);
 });
 // sort();
 
@@ -36,10 +47,10 @@ async function reverse(array) {
         array[i] = array[array.length - 1 - i];
         array[array.length - 1 - i] = temp;
         display(array);
-        await wait(speed.value ? speed.value : null);
+        await wait(null);
     }
 }
-async function sort(array) {
+async function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
         let swaps = 0;
         for (let j = 0; j < array.length - i - 1; j++) {
@@ -49,11 +60,31 @@ async function sort(array) {
                 array[j + 1] = temp;
                 display(array)
                 swaps++;
-                await wait(speed.value ? speed.value : null);
+                await wait(null);
             }
         }
         if (swaps === 0) break;
     }
+    handleFinish();
+}
+
+
+async function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let key = array[i];
+        let ptr = i - 1;
+        while (ptr >= 0 && key.value < array[ptr].value) {
+           
+            array[ptr + 1] = array[ptr];
+            ptr--;
+            display(array);
+            await wait(null);
+        }
+        array[ptr + 1] = key;
+        display(array);
+        await wait(null)
+    }
+    handleFinish();
 }
 
 function remove() {
@@ -72,8 +103,13 @@ function getArray(size) {
         const bar = document.createElement('bar');
         const value = Math.floor(Math.random() * 50) + 5;
         bar.style.height = `${value}px`;
+        bar.style.backgroundColor = `rgb(255,0,0, ${1 - 1 / 10})`;
         bar.className = 'bar';
         array.push({bar: bar, value: value });
     }
     return array;
+}
+
+function handleFinish() {
+    toSort = null;
 }
